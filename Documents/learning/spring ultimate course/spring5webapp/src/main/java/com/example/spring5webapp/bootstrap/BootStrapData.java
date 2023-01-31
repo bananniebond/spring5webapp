e6c074f602a776e51481260b6f73500f7eaa316e
anastasia.bondareva <anastasia.bondareva@williamhill.co.uk>
2023-01-31 11:37:16 +0000

    first commit

---
+++ Documents/learning/spring ultimate course/spring5webapp/src/main/java/com/example/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,56 @@
+package com.example.spring5webapp.bootstrap;
+
+import com.example.spring5webapp.domain.Author;
+import com.example.spring5webapp.domain.Book;
+import com.example.spring5webapp.domain.Publisher;
+import com.example.spring5webapp.repositories.AuthorRepository;
+import com.example.spring5webapp.repositories.BookRepository;
+import com.example.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+
+    private final BookRepository bookRepository;
+
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric","Evans");
+        Book ddd = new Book("Doman Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+
+        Author rod = new Author("Rod","Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "8589746");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: "+ bookRepository.count());
+
+
+
+
+    }
+}
