65a6deb186705518303f2a89e07c0d10177b8d09
anastasia.bondareva <anastasia.bondareva@williamhill.co.uk>
2023-02-02 11:21:23 +0000

    added h2 viewer

---
+++ Documents/learning/springultimate course/spring5webapp/src/main/java/com/example/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.spring5webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
